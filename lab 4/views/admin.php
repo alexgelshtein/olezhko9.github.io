3c72cf01df679f4c5a6e14503f409c3cacfa571a
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-04-05 02:08:16 +0300

    add basic admin panel

---
+++ lab 4/views/admin.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Админка</title>
+</head>
+
+<body>
+    <h1>Страница управления постами</h1>
+    <a href="index.php">Главная</a>
+    <br>
+    <div>
+        <table border="1">
+            <tr>
+                <th>id</th>
+                <th>title</th>
+                <th>date</th>
+                <th></th>
+            </tr>
+            <?php foreach($articles as $a): ?>
+            <tr>
+                <td><?=$a['id']?></td>
+                <td><?=$a['title']?></td>
+                <td><?=$a['created']?></td>
+                <td><a href="admin.php?action=delete&id=<?=$a['id']?>">Удалить</a></td>
+            </tr>
+            <?php endforeach ?>
+        </table>
+        <br>
+        <a href="index.php?action=add">Добавить запись</a>
+    </div>
+</body>
+
+</html>
