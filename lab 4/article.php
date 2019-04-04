48c778767fc008cf9fbf52231cfea282be4d0489
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-04-04 22:44:38 +0300

    add lab 4

---
+++ lab 4/article.php
@@ -0,0 +1,15 @@
+<?php
+
+function get_add_articles() {
+    
+}
+
+function get_article($article_id) {
+    
+}
+
+function add_article() {
+    
+}
+
+?>
---
+++ lab 4/views/artice.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Статья</title>
+</head>
+
+<body>
+    <div>
+        <h1></h1>
+        <em>Дата публицакии: </em>
+        <p></p>
+    </div>
+</body>
+
+</html>
