b1fd0a9cebe243fb8a7fd3db894c92876d21b947
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-04-17 17:34:46 +0300

    connection to mongoose

--- lab 5/server/src/config/config.js
+++ lab 5/server/src/config/config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    port: 9000
+    port: 9000,
+    dbURL: 'mongodb://localhost:27017/',
+    dbName: 'lab5',
+    dbOptions: { useNewUrlParser: true }
 };
--- lab 5/server/src/index.js
+++ lab 5/server/src/index.js
@@ -3,13 +3,22 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const config = require('./config/config');
+const mongoose = require('mongoose')
 
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(process.env.PORT || config.port, () => console.log(`Server started on port ${config.port}`));
+mongoose.Promise = global.Promise
+mongoose.connect(config.dbURL + config.dbName, config.dbOptions)
+
+mongoose.connection
+    .once('open', () => {
+        console.log(`Mongoose - successful connection`)
+        app.listen(process.env.PORT || config.port, () => console.log(`Server started on port ${config.port}`));
+    })
+    .on('error', error => console.warn(error))
 
 app.get('/markdowns', (req, res) => {
     const response = {
